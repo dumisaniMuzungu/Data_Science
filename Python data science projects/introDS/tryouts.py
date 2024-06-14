60c1703c697941b6e8f26db2bd5e224dec8b0fb5
dumisani <99218318+dumisaniMuzungu@users.noreply.github.com>
2024-06-14 14:55:49 +0200

    Add files via upload

---
+++ Python data science projects/introDS/tryouts.py
@@ -0,0 +1,10 @@
+def demoF(Name, Grade, Year, Programme):
+ return [{
+     "Name":  Name, "Grade": Grade, "Year": Year, "Programme": Programme,
+ }]
+
+
+demoE = demoF(input(print("Enter details: ", Name="",
+              Grade="", Year=int(""), Programme="")))
+
+print(demoE)
